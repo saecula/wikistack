c483d0c9dc2b3a09899293f1bd6bb1e9adffb59d
Amanda G <amagonzalez27@gmail.com>
2018-11-09 16:45:02 -0500

    we got so far

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,36 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
-const { Page } = require("../models");
-const { addPage } = require("../views");
+const { Page } = require('../models');
+const { addPage } = require('../views');
+const { wikipage } = require('../views');
 
 router.get('/', (req, res) => {
   res.send('blah');
 });
-
 
 router.post('/', async (req, res, next) => {
   console.log(req.body);
 
   const page = new Page({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
   });
 
   try {
-
     await page.save();
     console.log('page is ', page);
     let slug = page.dataValues.slug;
-    res.redirect(`/${slug}`);
+    res.redirect(`/wiki/${slug}`);
   } catch (error) {
-
     next(error);
   }
 });
 
-
 router.get('/add', (req, res) => {
   res.send(addPage());
 });
+
+router.get('/:slug', async (req, res) => {
+  const findSlug = await Page.findOne({ where: { slug: req.params.slug } });
+  console.log(findSlug);
+  // wikipage(findSlug, findSlug.)
+  res.json(findSlug);
+  // res.send(`hit dynamic route at ${req.params.slug}`);
+});
+
 module.exports = router;
